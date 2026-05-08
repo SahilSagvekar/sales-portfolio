b8e8f8787dfc605a4ded971123f07da92f12fe04
Sahil-Sagvekar <sahilsagvekar230@gmail.com>
2026-05-08 17:06:08 +0530

    fixes

--- lib/profiles.ts
+++ lib/profiles.ts
@@ -4,10 +4,10 @@ export interface Profile {
 }
 
 export const profiles: Record<string, Profile> = {
-  Kriszy:   { slug: "Kriszy",   name: "Kriszy Garcia"   },
-  Brylle:    { slug: "Brylle",    name: "Brylle Eullaran"     },
-  Jizanmark:   { slug: "Jizanmark",   name: "Jizanmark Hernaez"    },
-  MaryJoy:   { slug: "MaryJoy",   name: "Mary Joy Ramirez"    },
-  Hanelle:   { slug: "Hanelle",   name: "Hanelle Corullo"    },
+  kriszy:   { slug: "kriszy",   name: "Kriszy Garcia"   },
+  brylle:    { slug: "brylle",    name: "Brylle Eullaran"     },
+  jizanmark:   { slug: "jizanmark",   name: "Jizanmark Hernaez"    },
+  maryjoy:   { slug: "maryjoy",   name: "Mary Joy Ramirez"    },
+  hanelle:   { slug: "hanelle",   name: "Hanelle Corullo"    },
   // add more people here
 }
